21117ab90175b3f1abf9556c9b3f6c07f44c37a7
MuratovAS <lexamzx@gmail.com>
2023-02-01 16:32:44 +0500

    Update toolchain.sh

--- toolchain.sh
+++ toolchain.sh
@@ -69,7 +69,7 @@ cat $LOCAL_DIR/toolchain.txt | sed '/^#/d' | grep "toolchain-yosys" \
 cat $LOCAL_DIR/toolchain.txt | sed '/^#/d' | grep "tools-oss-cad-suite" \
 	&& if [ -d ".vscode" ]; then sed -i "s@\(\"verilog.linting.path\":\)[^,]*@\1 \"$TOOLCHAIN_PATH/tools-oss-cad-suite/bin/\"@" .vscode/settings.json; fi
 cat $LOCAL_DIR/toolchain.txt | sed '/^#/d' | grep "tools-oss-cad-suite" \
-	&& if [ -d ".vscode" ]; then sed -i "s@\(\"verilog.linting.iverilog.arguments\":\)[^,]*@\1 \"-B $TOOLCHAIN_PATH/tools-oss-cad-suite/lib/ivl $TOOLCHAIN_PATH/tools-oss-cad-suite/share/yosys/ice40/cells_sim.v src/top.v\"@" .vscode/settings.json; fi
+	&& if [ -d ".vscode" ]; then sed -i "s@\(\"verilog.linting.iverilog.arguments\":\)[^,]*@\1 \"-B $TOOLCHAIN_PATH/tools-oss-cad-suite/lib/ivl $TOOLCHAIN_PATH/tools-oss-cad-suite/share/yosys/ice40/cells_sim.v\"@" .vscode/settings.json; fi
 
 cat $LOCAL_DIR/toolchain.txt | sed '/^#/d' | grep "toolchain-sdcc" \
 	&& if [ -d ".vscode" ]; then sed -i "s@[$$\"].*/sdcc-4.0.0@\"$TOOLCHAIN_PATH/sdcc-4.0.0@" .vscode/c_cpp_properties.json; fi
